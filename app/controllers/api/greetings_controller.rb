171bd751262758f20240e40ec8955f659a8ad257
Hamza <hamzatarar8@gmail.com>
2022-06-01 10:42:04 +0200

    Edited controllers for front end.

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -2,7 +2,7 @@ module Api
   class GreetingsController < ApplicationController
     def index
       greeting = Greeting.order(Arel.sql('RANDOM()')).first
-      render json: { message: greeting.content }
+      render json: { message: greeting.message }
     end
   end
 end
